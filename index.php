efc958aa85cc504a960148f9f0ce80f5a7384ee0
zurika777 <44270254+zurika777@users.noreply.github.com>
2019-12-28 18:48:26 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:800&display=swap" rel="stylesheet">
+       <link rel="stylesheet" href="css/styles.css">
+
+    <title>izpsd</title>
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <img src="img/logo.png" alt="WAYUP">
+        <div class="offer">
+            <h1>Я УЧУ HTML5/CSS3</h1>
+            <p>И мне это дается легко!</p>
+        </div>
+    </div>
+</header>
+<section class="skills">
+    <div class="container">
+    <h2>Что мне даст знание верстки</h2>
+        <div class="merits">
+            <div class="unit">
+                <h3>Полный цикл работ</h3>
+                <p>Я смогу делать не только дизайн, но и верстку проектов для клиентов</p>
+            </div>
+            <div class="unit">
+                <h3>Постоянное развитие</h3>
+                <p>Верстка тесно связана с визуальным мир - она развивается ежедневно</p>
+            </div>
+            <div class="unit">
+                <h3>Больший заработок</h3>
+                <p>Верстка есно связана с визуальным мир - она развивается ежедневно</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gallery">
+    <div class="container">
+        <h2>Фото из моей будущей жизни</h2>
+        <div class="photos">
+            <img src="img/Photo1.jpg" alt="photo">
+            <img src="img/Photo2.jpg" alt="photo">
+            <img src="img/Photo3.jpg" alt="photo">
+        </div>
+        <a href="" class="btn">Стать верстальщиком</a>
+    </div>
+</section>
+<footer class="footer">
+    <div class="container">
+        <div class="footer-text">
+            <div class="footer-item">
+                WAYUP & Мое Имя damzadebulia.ge
+            </div>
+            <div class="footer-item">
+                Я готов стать лучше уже сегодня
+            </div>
+            <div class="footer-item">
+                Мир ждет меня
+            </div>
+
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
